e459c9647724354f3b1ef2f7deb741da76b5f0cc
Sean_Ware_Project <77515312+SeanWare5@users.noreply.github.com>
2025-11-03 22:19:52 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-document.getElementById('year')?.textContent=new Date().getFullYear();
+// small helpers
+document.getElementById('year')?.textContent = new Date().getFullYear();
+document.getElementById('year2')?.textContent = new Date().getFullYear();
+document.getElementById('year3')?.textContent = new Date().getFullYear();
